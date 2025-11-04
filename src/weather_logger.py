f5eadb789f3e477e5487be01925e56da85c647ea
venu-uppala <47878987+venu-uppala@users.noreply.github.com>
2025-11-04 21:19:45 +1100

    Create weather_logger.py

---
+++ src/weather_logger.py
@@ -0,0 +1,11 @@
+import logging
+
+def get_logger(name="melb_weather_data"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
