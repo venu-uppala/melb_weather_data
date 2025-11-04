91f22587cc82949a71bc50e4314a686786926b2b
venu-uppala <47878987+venu-uppala@users.noreply.github.com>
2025-11-04 23:22:40 +1100

    Update weather_config.py

--- src/weather_config.py
+++ src/weather_config.py
@@ -1,5 +1,5 @@
 import os
-
+# Postgress database configuration details
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "db"),
     "port": int(os.getenv("DB_PORT", 5432)),
@@ -7,6 +7,7 @@ DB_CONFIG = {
     "user": os.getenv("DB_USER", "postgres"),
     "password": os.getenv("DB_PASSWORD", "postgres")
 }
-
+# External weather data API URL
 API_URL = os.getenv("API_URL", "https://data.melbourne.vic.gov.au/api/explore/v2.1/catalog/datasets/microclimate-sensors-data/records")
+# Number of maximum records to fetch from the external API
 RECORD_LIMIT = int(os.getenv("RECORD_LIMIT", 1000))
