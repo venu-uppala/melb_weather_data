60506c51858367557899f5e67ee799cb326a0e71
venu-uppala <47878987+venu-uppala@users.noreply.github.com>
2025-11-04 21:29:44 +1100

    Create pipeline.py

---
+++ src/pipeline.py
@@ -0,0 +1,18 @@
+from src.weather_api import fetch_weather_data
+from src.weather_database import insert_weather_data
+from src.weather_logger import get_logger
+
+logger = get_logger()
+
+def run_weather_data_pipeline():
+    logger.info("Starting Weather data pipeline...")
+    data = fetch_weather_data()
+    
+    if not data:
+        logger.warning("No data fetched. Weather data load aborted.")
+        return
+    insert_weather_data(data)
+    logger.info("Weather data pipeline executed successfully.")
+
+if __name__ == "__main__":
+    run_weather_data_pipeline()
